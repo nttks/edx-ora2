99bdccd045d8b53bdb8839e2d601dc6882a136b4
JAST-TakumiKametani <axisware-754103@partner.jast.co.jp>
2024-08-01 06:19:20 +0000

    5908-makefileでミニファイ化したファイルのコミットも含む

--- openassessment/xblock/submission_mixin.py
+++ openassessment/xblock/submission_mixin.py
@@ -14,6 +14,9 @@ from validation import validate_submission
 from .resolve_dates import DISTANT_FUTURE
 from .user_data import get_user_preferences
 
+from openedx.core.djangoapps.ga_optional.api import is_available
+from openedx.core.djangoapps.ga_optional.models import ORA2_FILE_UPLOAD_SIZE_UP
+
 logger = logging.getLogger(__name__)
 
 
@@ -564,6 +567,7 @@ class SubmissionMixin(object):
             "text_response": self.text_response,
             "file_upload_response": self.file_upload_response,
             "prompts_type": self.prompts_type,
+            'file_size_up': is_available(ORA2_FILE_UPLOAD_SIZE_UP, self.course_id)
         }
 
         # Due dates can default to the distant future, in which case
